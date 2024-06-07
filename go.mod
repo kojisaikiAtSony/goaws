0aa2a1499d364a3cdb680d361a430af1355279a1
ksaiki <Koji.Saiki@sony.com>
2024-06-07 15:37:59 +0900

    add smoke test

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.18
 
 require (
 	github.com/aws/aws-sdk-go v1.47.3
-	github.com/aws/aws-sdk-go-v2 v1.25.2
+	github.com/aws/aws-sdk-go-v2 v1.27.1
 	github.com/aws/aws-sdk-go-v2/config v1.27.4
+	github.com/aws/aws-sdk-go-v2/service/sns v1.29.10
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.31.1
 	github.com/gavv/httpexpect/v2 v2.16.0
 	github.com/ghodss/yaml v1.0.0
@@ -22,15 +23,15 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.4 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.15.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.2 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.8 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.20.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.1 // indirect
-	github.com/aws/smithy-go v1.20.1 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
---
+++ smoke_tests/sns_create_topic_test.go
@@ -0,0 +1,253 @@
+package smoke_tests
+
+import (
+	"context"
+	"encoding/xml"
+	"net/http"
+	"testing"
+
+	"github.com/Admiral-Piett/goaws/app"
+	"github.com/Admiral-Piett/goaws/app/models"
+	"github.com/Admiral-Piett/goaws/app/utils"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateTopicV1_json_success(t *testing.T) {
+	server := generateServer()
+	defer func() {
+		server.Close()
+		utils.ResetResources()
+	}()
+
+	// Target test
+	topicName := "new-topic-1"
+	sdkConfig, _ := config.LoadDefaultConfig(context.TODO())
+	sdkConfig.BaseEndpoint = aws.String(server.URL)
+	snsClient := sns.NewFromConfig(sdkConfig)
+	sdkResponse, err := snsClient.CreateTopic(context.TODO(), &sns.CreateTopicInput{
+		Name: &topicName,
+	})
+
+	// Should success
+	assert.Contains(t, *sdkResponse.TopicArn, "new-topic-1")
+	assert.Nil(t, err)
+
+	// Get created topic
+	listTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+	}{
+		Action:  "ListTopics",
+		Version: "2012-11-05",
+	}
+	e := httpexpect.Default(t, server.URL)
+	r := e.POST("/").
+		WithForm(listTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 := app.ListTopicsResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Equal(t, 1, len(r2.Result.Topics.Member))
+	assert.Contains(t, r2.Result.Topics.Member[0].TopicArn, "new-topic-1")
+}
+
+func Test_CreateTopicV1_json_existant_topic(t *testing.T) {
+	server := generateServer()
+	defer func() {
+		server.Close()
+		utils.ResetResources()
+	}()
+
+	// Prepare existant topic
+	topicName := "new-topic-1"
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	sdkConfig.BaseEndpoint = aws.String(server.URL)
+	snsClient := sns.NewFromConfig(sdkConfig)
+	sdkResponse, _ := snsClient.CreateTopic(context.TODO(), &sns.CreateTopicInput{
+		Name: &topicName,
+	})
+	assert.Contains(t, *sdkResponse.TopicArn, "new-topic-1")
+	assert.Nil(t, err)
+
+	// Target test: create topic with same name
+	sdkResponse, err = snsClient.CreateTopic(context.TODO(), &sns.CreateTopicInput{
+		Name: &topicName,
+	})
+
+	// Should success
+	assert.Contains(t, *sdkResponse.TopicArn, "new-topic-1")
+	assert.Nil(t, err)
+
+	// Topic should not be duplicated
+	listTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+	}{
+		Action:  "ListTopics",
+		Version: "2012-11-05",
+	}
+	e := httpexpect.Default(t, server.URL)
+	r := e.POST("/").
+		WithForm(listTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 := app.ListTopicsResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Equal(t, 1, len(r2.Result.Topics.Member))
+	assert.Contains(t, r2.Result.Topics.Member[0].TopicArn, "new-topic-1")
+}
+
+func Test_CreateTopicV1_json_add_new_topic(t *testing.T) {
+	server := generateServer()
+	defer func() {
+		server.Close()
+		utils.ResetResources()
+	}()
+
+	// Prepare existant topic
+	topicName := "new-topic-1"
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+	sdkConfig.BaseEndpoint = aws.String(server.URL)
+	snsClient := sns.NewFromConfig(sdkConfig)
+	sdkResponse, _ := snsClient.CreateTopic(context.TODO(), &sns.CreateTopicInput{
+		Name: &topicName,
+	})
+	assert.Contains(t, *sdkResponse.TopicArn, "new-topic-1")
+	assert.Nil(t, err)
+
+	// Target test: create topic with same name
+	topicName2 := "new-topic-2"
+	sdkResponse, err = snsClient.CreateTopic(context.TODO(), &sns.CreateTopicInput{
+		Name: &topicName2,
+	})
+
+	// Should success
+	assert.Contains(t, *sdkResponse.TopicArn, "new-topic-2")
+	assert.Nil(t, err)
+
+	// Number of topic should be 2
+	listTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+	}{
+		Action:  "ListTopics",
+		Version: "2012-11-05",
+	}
+	e := httpexpect.Default(t, server.URL)
+	r := e.POST("/").
+		WithForm(listTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 := app.ListTopicsResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Equal(t, 2, len(r2.Result.Topics.Member))
+}
+
+func Test_CreateTopicV1_xml_success(t *testing.T) {
+	server := generateServer()
+	defer func() {
+		server.Close()
+		utils.ResetResources()
+	}()
+
+	// Target test
+	createTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+		Name    string `xml:"Name"`
+	}{
+		Action:  "CreateTopic",
+		Version: "2012-11-05",
+		Name:    "new-topic-1",
+	}
+	e := httpexpect.Default(t, server.URL)
+	r := e.POST("/").
+		WithForm(createTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 := models.CreateTopicResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Contains(t, r2.Result.TopicArn, "new-topic-1")
+
+	// Get created topic
+	listTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+	}{
+		Action:  "ListTopics",
+		Version: "2012-11-05",
+	}
+	r = e.POST("/").
+		WithForm(listTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r3 := app.ListTopicsResponse{}
+	xml.Unmarshal([]byte(r), &r3)
+	assert.Equal(t, 1, len(r3.Result.Topics.Member))
+	assert.Contains(t, r3.Result.Topics.Member[0].TopicArn, "new-topic-1")
+}
+
+func Test_CreateTopicV1_xml_existant_topic(t *testing.T) {
+	server := generateServer()
+	defer func() {
+		server.Close()
+		utils.ResetResources()
+	}()
+
+	// Prepare existant topic
+	createTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+		Name    string `xml:"Name"`
+	}{
+		Action:  "CreateTopic",
+		Version: "2012-11-05",
+		Name:    "new-topic-1",
+	}
+	e := httpexpect.Default(t, server.URL)
+	r := e.POST("/").
+		WithForm(createTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 := models.CreateTopicResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Contains(t, r2.Result.TopicArn, "new-topic-1")
+
+	// Target test: create topic with same name
+	r = e.POST("/").
+		WithForm(createTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r2 = models.CreateTopicResponse{}
+	xml.Unmarshal([]byte(r), &r2)
+	assert.Contains(t, r2.Result.TopicArn, "new-topic-1")
+
+	// Topic should not be duplicated
+	listTopicsXML := struct {
+		Action  string `xml:"Action"`
+		Version string `xml:"Version"`
+	}{
+		Action:  "ListTopics",
+		Version: "2012-11-05",
+	}
+	r = e.POST("/").
+		WithForm(listTopicsXML).
+		Expect().
+		Status(http.StatusOK).
+		Body().Raw()
+	r3 := app.ListTopicsResponse{}
+	xml.Unmarshal([]byte(r), &r3)
+	assert.Equal(t, 1, len(r3.Result.Topics.Member))
+	assert.Contains(t, r3.Result.Topics.Member[0].TopicArn, "new-topic-1")
+}
